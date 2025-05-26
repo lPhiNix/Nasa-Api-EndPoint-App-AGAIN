f27898743c9391c9660d0edadd41ec34771a6b62
lPhiNix <pablomargamer05@gmail.com>
2025-05-26 10:44:09 +0200

    Api Client Defined

--- Nasa-Api-EndPoint-App-AGAIN/Client/INasaApiClient.cs
+++ Nasa-Api-EndPoint-App-AGAIN/Client/INasaApiClient.cs
@@ -1,6 +1,8 @@
+using Nasa_Api_EndPoint_App_AGAIN.Dto;
+
 namespace Nasa_Api_EndPoint_App_AGAIN.Client;
 
 public interface INasaApiClient
 {
-   
+    Task<NasaApiResponse?> GetNeoFeedAsync(DateTime start, DateTime end);
 }
--- Nasa-Api-EndPoint-App-AGAIN/Client/NasaApiClient.cs
+++ Nasa-Api-EndPoint-App-AGAIN/Client/NasaApiClient.cs
@@ -1,6 +1,22 @@
+using Nasa_Api_EndPoint_App_AGAIN.Dto;
+using Newtonsoft.Json;
+
 namespace Nasa_Api_EndPoint_App_AGAIN.Client;
 
-public class NasaApiClient : INasaApiClient
+public class NasaApiClient(HttpClient httpClient) : INasaApiClient
 {
-   
+    private const string ApiKey = "DEMO_KEY";
+    private const string BaseUrl = "https://api.nasa.gov/neo/rest/v1/feed";
+
+    public async Task<NasaApiResponse?> GetNeoFeedAsync(DateTime start, DateTime end)
+    {
+        var url = $"{BaseUrl}?start_date={start:yyyy-MM-dd}&end_date={end:yyyy-MM-dd}&api_key={ApiKey}";
+        var response = await httpClient.GetAsync(url);
+
+        if (!response.IsSuccessStatusCode)
+            throw new Exception("Error to consult NASA API");
+
+        var json = await response.Content.ReadAsStringAsync();
+        return JsonConvert.DeserializeObject<NasaApiResponse>(json);
+    }
 }
